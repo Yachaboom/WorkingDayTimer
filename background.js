c44f361d460a6c4a976628392685add4a41d54a9
yachaboom <yachaboom@oneunivrs.com>
2023-05-11 10:25:10 +0900

    출퇴근 모니터링 요소 제거

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 calendarTab = null;
-infoTab = null;
 
 function UpdateWorkTimer(tab) {
     if (tab.url == "https://gwa.oneunivrs.com/gw/userMain.do") {
@@ -12,26 +11,12 @@ function UpdateWorkTimer(tab) {
     }
 }
 
-function SendEnterExitState(tab) {
-    if (tab.url == "https://gwa.oneunivrs.com/gw/userMain.do") {
-        chrome.scripting.executeScript({
-            target: {
-                tabId: tab.id
-            },
-            files: ['notion.js']
-        });
-    }
-}
-
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
     if (tab.url == "https://gwa.oneunivrs.com/gw/userMain.do") {
         if (changeInfo.status == "complete") {
             calendarTab = null;
             openCalendarTab("https://gwa.oneunivrs.com/yjmgames/ft/ftCalendarForm");
-
-            infoTab = null;
-            openInfoTab("https://gwa.oneunivrs.com/gw/cmm/systemx/myInfoManage.do");
         }
     }
 })
@@ -53,25 +38,6 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     }
 });
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (infoTab != null, request.message === 'info_loaded' && sender.tab.id == infoTab.id) {
-        const notionName = request.content.name;
-        const notionTeam = request.content.team;
-        chrome.storage.local.set({ notionName, notionTeam });
-        chrome.tabs.remove(sender.tab.id);
-
-        chrome.tabs.query({ url: "https://gwa.oneunivrs.com/gw/userMain.do" }, (tabs) => {
-            if (tabs.length === 0) {
-                return;
-            }
-
-            for (let i = 0 ; i < tabs.length ; ++i) {
-                SendEnterExitState(tabs[i]);
-            }
-        });     
-    }
-});
-
 function openCalendarTab(url) {
     chrome.tabs.create({ url: url, active: false }, onOpenCalendarTab);
 }
@@ -83,17 +49,3 @@ function onOpenCalendarTab(tab) {
 
     calendarTab = tab;
 }
-
-function openInfoTab(url) {
-    chrome.tabs.create({ url: url, active: false }, onOpenInfoTab);
-}
-
-function onOpenInfoTab(tab) {
-    if (infoTab != null) {
-        chrome.tabs.remove(infoTab.id);
-    }
-
-    infoTab = tab;
-}
-
-
